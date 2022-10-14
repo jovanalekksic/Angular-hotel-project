f44df1f4ab3281853fd6fcca57a9326883d7ab67
jovanalekksic <jaleksic23@gmail.com>
2022-10-14 15:22:02 +0200

    first commit

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+// let lname: string;
+// lname = 'Santosh';
+// let newname = lname.toUpperCase();
+let c = 2 /* Color.Blue */;
---
+++ dist/datatypes.js
@@ -0,0 +1,32 @@
+"use strict";
+// let lname: string;
+// lname = 'Santosh';
+// let newname = lname.toUpperCase();
+// console.log(newname);
+// let age: number;
+// age = 23;
+// let dob = "26";
+// let result = parseInt(dob)
+// console.log(typeof (result));
+// let isValid: boolean = false;
+// console.log(isValid);
+// let emplist: string[];
+// emplist = ["Jovana", "Nikola", "Irina"];
+// let numList: Array<number>;
+// numList = [1, 2, 3, 4, 5];
+// let results = numList.filter((num) => num > 2);
+// console.log(results);
+// let sum = numList.reduce((acc, num) => acc + num);
+// console.log(sum);
+// const enum Color {
+//     Red,
+//     Green,
+//     Blue
+// }
+// let c: Color = Color.Blue; 
+let swapNumbs;
+function swapNumbers(num1, num2) {
+    return [num2, num1];
+}
+swapNumbs = swapNumbers(10, 20);
+swapNumbs[0];
